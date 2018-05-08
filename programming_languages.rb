3daf6d9917519ce5c7c8c7f33d7f5e068e2d3330
Anthony Lam <al3101@nyu.edu>
2018-05-08 18:12:32 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,5 +1,6 @@
 def reformat_languages(languages)
   new_hash = {} 
   languages.each do |style, language_data| 
-    language_data.each do |language, 
+    language_data.each do |language, data|
+      new_hash[language] =
 end
